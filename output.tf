470f3c0f0319446610a96168090d891b6fe22d44
learndaws <learndaws@gmail.com>
2024-06-16 17:51:46 +0530

    readme

--- output.tf
+++ output.tf
@@ -2,3 +2,18 @@ output "roboshop_us_east_1" {
     value = data.aws_availability_zones.roboshop_us_east_1_avail_az
 }
 
+output "vpc_id" {
+  value = aws_vpc.developer.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.roboshop_public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.roboshop_private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.roboshop_database[*].id
+}
